4e73da9ab85079a2c9619581d02928883422b544
antonpalinau <anton_palinau@epam.com>
2020-05-20 14:57:36 +0300

    added code splitting

--- client/src/components/with-spinner/with-spinner.component.jsx
+++ client/src/components/with-spinner/with-spinner.component.jsx
@@ -1,14 +1,12 @@
-import React from 'react'
-import { SpinnerOverlay, SpinnerContainer } from './with-spinner.styles';
+import React from "react";
+import Spinner from "../spinner/spinner.component";
 
-const WithSpinner = WrappedComponent => ({ isLoading, ...otherProps }) => {
-    return isLoading ? (
-        <SpinnerOverlay>
-            <SpinnerContainer />
-        </SpinnerOverlay>
-    ) : (
-        <WrappedComponent {...otherProps} />
-    )
-}
+const WithSpinner = (WrappedComponent) => ({ isLoading, ...otherProps }) => {
+  return isLoading ? (
+    <Spinner />
+  ) : (
+    <WrappedComponent {...otherProps} />
+  );
+};
 
 export default WithSpinner;
