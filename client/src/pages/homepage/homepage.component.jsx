547d3d4bee712141aedd9ae0c73378b335318667
antonpalinau <anton_palinau@epam.com>
2020-05-20 15:17:58 +0300

    added profiler example

--- client/src/pages/homepage/homepage.component.jsx
+++ client/src/pages/homepage/homepage.component.jsx
@@ -1,12 +1,23 @@
-import React from 'react';
+import React, { Profiler } from "react";
 
-import { HomepageContainer } from './homepage.styles';
-import Directory from '../../components/directory/directory.components';
+import { HomepageContainer } from "./homepage.styles";
+import Directory from "../../components/directory/directory.components";
 
 const HomePage = () => (
-    <HomepageContainer>
-        <Directory />
-    </HomepageContainer>
+  <HomepageContainer>
+    <Profiler
+      id="Directory"
+      onRender={(id, phase, actualDuration) => {
+        console.log({
+          id,
+          phase,
+          actualDuration,
+        });
+      }}
+    >
+      <Directory />
+    </Profiler>
+  </HomepageContainer>
 );
 
 export default HomePage;
